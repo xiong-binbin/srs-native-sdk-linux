0e88b5513ff1b48c923c8a2a4a9af2bf3633b39e
xiong-binbin <qq283302629@gmail.com>
2022-08-02 08:27:20 +0800

    update

--- src/srs_client.cpp
+++ src/srs_client.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "srs_client.h"
+#include "webrtc/rtc_base/ssl_adapter.h"
+#include "webrtc/system_wrappers/include/field_trial.h"
+
+SrsClient::SrsClient()
+{
+}
+
+SrsClient::~SrsClient()
+{
+}
+
+int SrsClient::Initialize()
+{
+    webrtc::field_trial::InitFieldTrialsFromString("");
+
+    CustomSocketServer socketServer;
+    rtc::AutoSocketServerThread thread(&socketServer);
+
+    rtc::InitializeSSL();
+}
--- src/srs_client.h
+++ src/srs_client.h
@@ -5,9 +5,38 @@
 #ifndef SRS_NATIVE_SDK_LINUX_SRS_CLIENT_H
 #define SRS_NATIVE_SDK_LINUX_SRS_CLIENT_H
 
+#include <iostream>
+#include "webrtc/rtc_base/thread.h"
+#include "webrtc/rtc_base/physical_socket_server.h"
 
-class SrsClient {
 
+class CustomSocketServer : public rtc::PhysicalSocketServer
+{
+public:
+    explicit CustomSocketServer() {}
+    virtual ~CustomSocketServer() {}
+
+    virtual void SetMessageQueue(rtc::Thread* queue) override { msgQueue = queue; }
+
+    virtual bool Wait(int cms, bool process_io) override
+    {
+        return rtc::PhysicalSocketServer::Wait(0, process_io);
+    }
+
+private:
+    rtc::Thread* msgQueue;
+};
+
+
+class SrsClient
+{
+public:
+    SrsClient();
+    ~SrsClient();
+
+    int Initialize();
+
+private:
 };
 
 
