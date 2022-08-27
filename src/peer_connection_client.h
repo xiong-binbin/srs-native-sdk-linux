e9530fd61e667b6ea5724b21b101ffad631954b5
xiong-binbin <qq283302629@gmail.com>
2022-08-27 11:59:01 +0800

    update

--- src/peer_connection_client.h
+++ src/peer_connection_client.h
@@ -42,6 +42,8 @@ public:
     PeerConnectionClient();
     ~PeerConnectionClient();
 
+    void RegisterObserver(PeerConnectionClientObserver* callback);
+
     bool is_connected();
 
     void Connect(const std::string& server, int port, const std::string& client_name);
@@ -50,8 +52,47 @@ public:
 
     bool SendHangUp(int peer_id);
 
+protected:
+    void Close();
+    bool ConnectControlSocket();
+    void DoConnect();
+    void OnResolveResult(rtc::AsyncResolverInterface* resolver);
+
+    void OnClose(rtc::AsyncSocket* socket, int err);
+    void OnConnect(rtc::AsyncSocket* socket);
+    void OnRead(rtc::AsyncSocket* socket);
+
+    void OnHangingGetConnect(rtc::AsyncSocket* socket);
+    void OnHangingGetRead(rtc::AsyncSocket* socket);
+
+    void OnMessageFromPeer(int peer_id, const std::string& message);
+
+    bool ReadIntoBuffer(rtc::AsyncSocket* socket, std::string* data, size_t* content_length);
+
+    // Quick and dirty support for parsing HTTP header values.
+    bool GetHeaderValue(const std::string& data, size_t eoh, const char* header_pattern, size_t* value);
+    bool GetHeaderValue(const std::string& data, size_t eoh, const char* header_pattern, std::string* value);
+
+    int  GetResponseStatus(const std::string& response);
+    bool ParseServerResponse(const std::string& response, size_t content_length, size_t* peer_id, size_t* eoh);
+
+    // Parses a single line entry in the form "<name>,<id>,<connected>"
+    bool ParseEntry(const std::string& entry, std::string* name, int* id, bool* connected);
+
 private:
     int id_ = -1;
+    State state_ = NOT_CONNECTED;
+    std::string client_name_;
+    std::string onconnect_data_;
+    std::string control_data_;
+    std::string notification_data_;
+    std::map<int, std::string> peers_;
+    PeerConnectionClientObserver* callback_ = nullptr;
+
+    rtc::SocketAddress server_address_;
+    rtc::AsyncResolver* resolver_;
+    std::unique_ptr<rtc::AsyncSocket> control_socket_;
+    std::unique_ptr<rtc::AsyncSocket> hanging_get_;
 };
 
 
